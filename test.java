4a1be24c4cdfd27dab8d59d46f7e83cd928ccd58
simplyesu <simplyesu@gmail.com>
2019-11-24 01:34:04 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,9 @@
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class test {
+  public static void main(String[] args) {
+    System.out.println("This is the test folder");
+
+  }
+}
